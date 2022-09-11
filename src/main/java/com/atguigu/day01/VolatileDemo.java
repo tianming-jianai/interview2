0e8da77238163d1c62bb37a07722023a63ddd540
zsg1001 <1724473740@qq.com>
2022-09-11 11:26:08 +0800

    volatile 不保证原子性

--- src/main/java/com/atguigu/day01/VolatileDemo.java
+++ src/main/java/com/atguigu/day01/VolatileDemo.java
@@ -8,9 +8,33 @@ package com.atguigu.day01;
  * @Description:
  * 1. 验证volatile的可见性
  * 1.1 假设 int number = 0; number 变量之前根本没有添加volatile关键字修饰，没有可见性
+ * 1.2 添加了 volatile，可以解决可见性问题
+ *
+ * 2. 验证 volatile不保证原子性
+ * 2.1 原子性指的是什么
+ *      不可分割，完整性，也即某个线程正在做某个具体业务时，中间不可以被加塞或者被分割，需要整体完整要么同时成功，要么同时失败
+ * 2.2 volatile 不保证原子性的案例演示
  */
 public class VolatileDemo {
+
     public static void main(String[] args) {// main是一切方法入口
+        MyData myData = new MyData();
+        for (int i = 1; i <= 20; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 1000; j++) {
+                    myData.addPlusPlus();
+                }
+            }, String.valueOf(i)).start();
+        }
+        // 需要等待上面20个线程都全部计算完成后，再用main线程取得最终的结果值看是多少？
+        while (Thread.activeCount() > 2) {
+            Thread.yield();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t finally number value：" + myData.number);
+    }
+
+    // volatile 可以保证可见性，及时通知其它线程，主物理内存的值已经被修改
+    private static void seekOkByVolatile() {
         MyData myData = new MyData();// 资源类
 
         new Thread(() -> {
@@ -37,5 +61,10 @@ class MyData {
 
     public void addNumTo60() {
         this.number = 60;
+    }
+
+    // 注意：此时number前面是加了volatile关键字修饰的，volatile不保证原子性
+    public void addPlusPlus() {
+        this.number++;
     }
 }
